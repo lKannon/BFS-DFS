2d56608785211a7f10a98d2506499fa1f9dc0d17
lKannon <andrew.p.borges@gmail.com>
2023-03-25 18:10:14 -0400

    Modificação de print apenas

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from states import *
 print("Insira o vetor de Estados [ x, x, x] de acordo com:")
 print("Classe de estados possíveis")
 print("Os Estados possiveis sao dados por um vetor de 3 elementos no qual:")
-print("1 Elemento -> position -> Posiçao do robô")
+print("1 Elemento -> position -> Posiçao do robô (0 ou 1)")
 print("2 Elemento -> statusR1 -> Status da sala 1 (Limpa ou Suja, 0 ou 1)")
 print("3 Elemento -> statusR2 -> Status da sala 2 (Limpa ou Suja, 0 ou 1)\n")
 state = []
